9ea9a0b496601af35ba893347da56b5bccc9f432
DR. KAMALJIT SINGH SAINI <sainikamaljitsingh@gmail.com>
2024-01-24 11:53:11 +0530

    version2

--- binarySearch.py
+++ binarySearch.py
@@ -1,18 +1,18 @@
-def binary_search(arr, down, high, x):
+def binary_search(arr1, down, high, x):
     if high >= down:
         mid = (high + down) // 2
-        if arr[mid] == x:
+        if arr1[mid] == x:
             return mid
-        elif arr[mid] > x:
-            return binary_search(arr, down, mid - 1, x)
+        elif arr1[mid] > x:
+            return binary_search(arr1, down, mid - 1, x)
         else:
-            return binary_search(arr, mid + 1, high, x)
+            return binary_search(arr1, mid + 1, high, x)
     else:
         return -1
 
-arr = [2, 3, 4, 10, 40]
+arr1 = [2, 3, 4, 10, 40]
 x = 10
-result = binary_search(arr, 0, len(arr)-1, x)
+result = binary_search(arr1, 0, len(arr1)-1, x)
 
 if result != -1:
     print("Element is present at index", str(result))
